8844935009bb8c8e4bb8adb7a39da4a02639a002
louye <louye@yeteam.com>
2020-08-18 11:57:46 +0800

    add: 函数式编程

--- retriever/main.go
+++ retriever/main.go
@@ -11,13 +11,40 @@ type Retriever interface {
 	Get(url string) string
 }
 
+type Poster interface {
+	Post(url string, form map[string]string) string
+}
+
+const url = "http://www.imooc.com"
+
 func download(r Retriever) string {
-	return r.Get("http://www.imooc.com")
+	return r.Get(url)
+}
+
+func post(poster Poster) {
+	poster.Post(url,
+		map[string]string{
+			"name":   "ccmouse",
+			"course": "golang",
+		})
+}
+
+type RetrieverPost interface {
+	Retriever
+	Poster
+}
+
+func session(s RetrieverPost) string {
+	s.Post(url, map[string]string{
+		"content": "another fake imooc.com",
+	})
+	return s.Get(url)
 }
 
 func main() {
 	var r Retriever
-	r = mock.Retriever{Content: "this is a fake imooc.com"}
+	retriever := mock.Retriever{Content: "this is a fake imooc.com"}
+	r = &retriever
 	inspect(r)
 	r = &real.Retriever{
 		UserAgent: "clf",
@@ -28,15 +55,20 @@ func main() {
 	fmt.Println("Type assertion: ")
 	realRetriever := r.(*real.Retriever)
 	fmt.Println(realRetriever.TimeOut)
+
+	fmt.Println("Try Session: ")
+	fmt.Println(session(&retriever))
 }
 
 func inspect(r Retriever) {
-	fmt.Println("Type Switch: ")
-	fmt.Printf("%T %v\n", r, r)
+	fmt.Println("Inspecting:", r)
+	fmt.Printf(" > %T %v\n", r, r)
+	fmt.Println(" > Type Switch: ")
 	switch v := r.(type) {
-	case mock.Retriever:
+	case *mock.Retriever:
 		fmt.Println("Content: ", v.Content)
 	case *real.Retriever:
 		fmt.Println("UserAgent: ", v.UserAgent)
 	}
+	fmt.Println()
 }
--- retriever/mock/mock.go
+++ retriever/mock/mock.go
@@ -1,9 +1,20 @@
 package mock
 
+import "fmt"
+
 type Retriever struct {
 	Content string
 }
 
-func (r Retriever) Get(url string) string {
+func (r *Retriever) String() string {
+	return fmt.Sprintf("Retriever:{Content:%s}", r.Content)
+}
+
+func (r *Retriever) Post(url string, form map[string]string) string {
+	r.Content = form["content"]
+	return "ok"
+}
+
+func (r *Retriever) Get(url string) string {
 	return r.Content
 }
